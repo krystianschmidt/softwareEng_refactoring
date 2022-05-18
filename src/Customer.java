e9bb329737f1d7e2bd2923794875b4e031606bbc
Krystian Schmidt <krystian.schmidt@siemens.com>
2022-05-18 19:51:35 +0200

    refactor syntax (semicolons, indentations)

--- src/Customer.java
+++ src/Customer.java
@@ -3,21 +3,25 @@ import java.lang.*;
 import java.util.*;
 
 class Customer {
+
     private String name;
     private Vector rentals = new Vector();
-    public Customer (String newname){
+
+    public Customer (String newname) {
         name = newname;
-    };
+    }
+
     public void addRental(Rental arg) {
         rentals.addElement(arg);
-    };
+    }
+
     public String getName (){
         return name;
     };
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
-        Enumeration enum_rentals = rentals.elements();	    
+        Enumeration enum_rentals = rentals.elements();
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
